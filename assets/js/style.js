991bc2f11642ee1fcce63ac699fd681587065de2
Mivinci <1366723936@qq.com>
2021-11-06 15:28:56 +0800

    some styles fixes

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,6 +1,9 @@
-// This file contains some codes to fix style of elements under `.md`
+// fix style of checkboxes in posts.
 document.querySelectorAll('.md ul').forEach(v => {
   if (/<li><input .+>.+<\/li>/.test(v.innerHTML)) {
     v.classList.add('ul-checkbox');
   }
 });
+
+// use custom font family
+document.querySelector('body').style.setProperty('--global-font-family', '{{ .Site.Params.globalFontFamily }}');
