d4d30a0c3536657825f8be7f2e1d22dff3294bb1
Mivinci <1366723936@qq.com>
2022-01-26 20:39:49 +0800

    authors now can pick a default theme color

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,29 +1,28 @@
-const light = '{{ index .Site.Params.switch 1 }}'
-const dark = '{{ index .Site.Params.switch 0 }}'
+const icon_light = '{{ index .Site.Params.switch 1 }}'
+const icon_dark = '{{ index .Site.Params.switch 0 }}'
 const comment = '{{ .Site.Params.comment }}'
-const LIGHT = 'light', DARK = 'dark'
+const light = 'light', dark = 'dark'
 
 const themeSwitcher = document.getElementById('theme-switcher')
 
 // set switcher
-themeSwitcher.innerHTML = localStorage.theme === LIGHT ? light : dark
+themeSwitcher.innerHTML = localStorage.theme === light ? icon_light : icon_dark
 
 themeSwitcher.addEventListener('click', function () {
   const currentTheme = localStorage.theme
-  const newTheme = currentTheme === LIGHT ? DARK : LIGHT
+  const newTheme = currentTheme === light ? dark : light
   
   // switch global theme
   switchMinimaTheme(currentTheme, newTheme)
 
   // switch utterance theme if necessary
-
   if (comment === 'utterances')
     switchUtteranceTheme(`github-${newTheme}`)
 });
 
 function switchMinimaTheme(oldTheme, newTheme) {
   const { classList } = document.documentElement
-  const text = newTheme === LIGHT ? light : dark;
+  const text = newTheme === light ? icon_light : icon_dark;
   
   classList.remove(oldTheme);
   classList.add(newTheme);
