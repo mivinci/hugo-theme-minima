f935dbc74fb25984fdd35d6e40b6a277528b2a07
mivinci <mivinci@qq.com>
2023-03-14 18:00:05 +0800

    replace style.js with a CSS trick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 import { setup_theme_switch } from "./theme"
-import { fix_checkbox_list_style } from "./style"
 import { setup_selectable } from './selectable'
 
 
 setup_theme_switch('theme-switch')
 setup_selectable()
-fix_checkbox_list_style('.md ul')
--- assets/js/style.js
+++
@@ -1,8 +0,0 @@
-/** @param {string} selectors */
-export function fix_checkbox_list_style(selectors) {
-  document.querySelectorAll(selectors).forEach(v => {
-    if (/<li><input .+>.+<\/li>/.test(v.innerHTML)) {
-      v.classList.add('ul-checkbox');
-    }
-  });
-}
